6a50bdc4163c7c11ed103e7b05a045d0f776b177
SANTIAGO GIRALDO GONZALEZ <santiagogiraldo204794@correo.itm.edu.co>
2021-09-04 16:53:29 -0500

    Add vehicle Type

---
+++ Vehicles.API/Data/Entities/VehicleType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicles.API.Data.Entities
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+        
+        [Display(Name ="Tipo en vehículo")]
+        [MaxLength(50, ErrorMessage ="El campo {0} no puede tener mas de {1} carácteres.")]
+        [Required(ErrorMessage ="El Campo {0} es obligatorio.")]
+        public string Description { get; set; }
+
+        
+
+    }
+}
